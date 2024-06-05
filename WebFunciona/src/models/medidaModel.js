3eb53796effb14c320aa22dfb62622fe7e0504ff
EduardoMiyasaki <eduardomiyasaki22@gmail.com>
2024-06-05 14:18:57 -0300

    Plotando na dashbord de quiz

--- WebFunciona/src/models/medidaModel.js
+++ WebFunciona/src/models/medidaModel.js
@@ -1,29 +1,22 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
+function buscarUltimasMedidas(idUsuario) {
 
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    FROM medida
-                    WHERE fk_aquario = ${idAquario}
-                    ORDER BY id DESC LIMIT ${limite_linhas}`;
+    var instrucaoSql = `SELECT Round((Sum(questoesCorretas)/Sum(questoes))*100) as porcentagem, 
+    Quiz.questoesCorretas,
+    Quiz.questoes
+     from Quiz where fkUsuario = ${idUsuario}
+     group by questoesCorretas,questoes
+     Order by questoesCorretas,questoes;`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
+function buscarMedidasEmTempoReal(idUsuario) {
 
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
-                    ORDER BY id DESC LIMIT 1`;
+    var instrucaoSql = `SELECT Round((Sum(questoesCorretas)/Sum(questoes))*100)
+    from Quiz where fkUsuario = ${idUsuario};`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
